a545d7024ed14482ac691999eec9fe67d28c26fb
Manuel <31109774+Megalight09@users.noreply.github.com>
2019-03-01 11:47:10 -0400

    Elimina opción de unidad de período.

--- Financiera/Formularios/Opciones/A/TotalPeriodos.cs
+++ Financiera/Formularios/Opciones/A/TotalPeriodos.cs
@@ -7,20 +7,9 @@ namespace Financiera.Formularios
 {
     public partial class TotalPeriodos_A : UserControl
     {
-        private static IReadOnlyDictionary<string, string> Periodos { get; } = new Dictionary<string, string>{
-            {"Año(s)", "ANUAL"}, {"Semestre(s)", "SEMESTRAL"}, {"Cuatrimestre(s)", "CUATRIMESTRAL"},
-            { "Trimestre(s)", "TRIMESTRAL"}, {"Bimestre(s)", "BIMESTRAL"},
-            { "Mes(es)", "MENSUAL"}, {"Quincena(s)", "QUINCENAL"}, {"Semana(s)", "SEMANAL"}, {"Día(s)", "DIARIA"}
-        };
-
         public TotalPeriodos_A()
         {
             InitializeComponent();
-            foreach(var periodo in Periodos.Keys)
-            {
-                cbPeriodos.Items.Add(periodo);
-            }
-            cbPeriodos.SelectedIndex = cbPeriodos.Items.IndexOf("Año(s)");
         }
 
         private void btnCalcular_Click(object sender, EventArgs e)
@@ -29,11 +18,6 @@ namespace Financiera.Formularios
             decimal periodo = txtPeriodo.Value;
             txtResultado.Value = InteresCompuesto.CalcularTotalPeriodos(periodo, tiempo);
             panelResultado.Visible = true;
-        }
-
-        private void cbPeriodos_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            labelPeriodo.Text = cbPeriodos.SelectedItem.ToString();
         }
     }
 }
